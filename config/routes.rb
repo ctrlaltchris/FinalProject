82188761ad84fc81aa77e31e029501836e75b231
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-07-20 21:24:52 +1000

    sign up section incorporated

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'sites#order'
 
+  get '/users' => 'devise/registrations#new'
+
   post '/result' => 'sites#order'
 
   get '/api'=> 'sites#api'
