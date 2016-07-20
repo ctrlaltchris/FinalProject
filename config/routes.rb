58acd9e3ed1e7a8bfb21f549f24e825c826a2c4d
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-07-20 20:41:37 +1000

    Order page styling added

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sites#index'
+  root 'sites#order'
 
   post '/result' => 'sites#order'
 
