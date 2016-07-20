4cead9ea6a3acc4d8ca68d8978f4ec14c4c9c247
Mark Cronje <markcronje@Marks-MacBook-Air.local>
2016-07-20 17:56:15 +1000

    Changes before merge

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -6,6 +6,17 @@ $(document).ready(function(){
 
  $('div.s6').leanModal();
 
-
+new domainr.SearchBox({
+  mashapeKey: 'rZgC4DqgNbmsh9mKxshkDrGd4pdYp1W1pP6jsnopatNptaoljs',
+  // clientId: 'yourClientId', // or alternatively your clientId (for high-volume customers), in which case you don't need a Mashape key
+  observe: document.getElementById('search'),
+  renderTo: document.getElementById('results'),
+  limit: 10, // limit to 10 results
+  registrar: 'namecheap.com', // only return a registrar's supported TLDs
+  defaults: [ 'coffee', 'rocks', 'co', 'io' ], // include these TLDs by default
+  onSelect: function(result) {
+    window.open('https://api.domainr.com/v2/register?registrar=namecheap.com&domain=' + result.domain);
+  }
+});
 
 });
